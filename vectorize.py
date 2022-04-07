e9229cc97d0a78f1c7d82dc88dc8f3b30bffac32
Mikolaj Figurski <meek.f80@gmail.com>
2022-04-07 16:35:18 -0400

    Perform vectorization

--- vectorize.py
+++ vectorize.py
@@ -14,10 +14,19 @@ def tokenize(l):
     # TODO: implement bi-gram detector model
 
 
+def vectorize(l):
+    model = gensim.downloader.load("glove-twitter-25")
+    #  print(model)
+    return [[list(model[token]) for token in tweet if token in model] for tweet in l]
+
+
 if __name__ == "__main__":
     df_iter = pd.read_csv(
-        "./data/smol.csv", header=None, chunksize=100, encoding="ISO-8859-1"
+        "./data/smol.csv", header=None, chunksize=1000, encoding="ISO-8859-1"
     )
+    mode = "w"
     for df in df_iter:
-        print(tokenize(df[5]))
-        break
+        vecs = pd.DataFrame(vectorize(tokenize(df[5])))
+        vecs.to_csv("./data/vectorized-smol.csv", header=None, index=False, mode=mode)
+        print("...")
+        mode = "a"
