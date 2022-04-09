b5dc0541c8f2dc8763413f852c57b40d68bdeb42
Mikolaj Figurski <meek.f80@gmail.com>
2022-04-08 21:02:46 -0400

    Standardize vectorization to 30 words

--- vectorize.py
+++ vectorize.py
@@ -2,13 +2,14 @@ import gensim.downloader
 import pandas as pd
 import string
 
-stoplist = set("for a and of the in to so".split())
+STOPLIST = set("for a and of the in to so".split())
+CHUNK_SIZE = 1000
 
 
 def tokenize(l):
     trans = str.maketrans("", "", string.punctuation)
     return [
-        [w.translate(trans) for w in tweet.lower().split() if w not in stoplist]
+        [w.translate(trans) for w in tweet.lower().split() if w not in STOPLIST]
         for tweet in l
     ]
     # TODO: implement bi-gram detector model
@@ -17,16 +18,18 @@ def tokenize(l):
 def vectorize(l):
     model = gensim.downloader.load("glove-twitter-25")
     #  print(model)
-    return [[list(model[token]) for token in tweet if token in model] for tweet in l]
+    return [([list(model[token]) for token in tweet if token in model] + [None]*30)[:30] for tweet in l]
 
 
 if __name__ == "__main__":
+    n_done = 0
     df_iter = pd.read_csv(
-        "./data/smol.csv", header=None, chunksize=1000, encoding="ISO-8859-1"
+        "./data/smol.csv", header=None, chunksize=CHUNK_SIZE, encoding="ISO-8859-1"
     )
     mode = "w"
     for df in df_iter:
         vecs = pd.DataFrame(vectorize(tokenize(df[5])))
         vecs.to_csv("./data/vectorized-smol.csv", header=None, index=False, mode=mode)
-        print("...")
+        n_done += CHUNK_SIZE
         mode = "a"
+        print(f"{n_done}...")
