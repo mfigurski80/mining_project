2372cbc0d886d6f0b1a2633bac60053f77616cbe
Mikolaj Figurski <meek.f80@gmail.com>
2022-04-16 17:06:22 -0400

    Remove usernames in tokenization

--- vectorize.py
+++ vectorize.py
@@ -7,9 +7,14 @@ CHUNK_SIZE = 1000
 
 
 def tokenize(l):
+
     trans = str.maketrans("", "", string.punctuation)
     return [
-        [w.translate(trans) for w in tweet.lower().split() if w not in STOPLIST]
+        [
+            w.translate(trans)
+            for w in tweet.lower().split()
+            if (w not in STOPLIST and not w.startswith("@"))
+        ]
         for tweet in l
     ]
     # TODO: implement bi-gram detector model
@@ -17,8 +22,15 @@ def tokenize(l):
 
 def vectorize(l):
     model = gensim.downloader.load("glove-twitter-25")
-    #  print(model)
-    return [([list(model[token]) for token in tweet if token in model] + [None]*30)[:30] for tweet in l]
+    return [[list(model[token]) for token in tweet if token in model] for tweet in l]
+
+
+def make_even(items, n):
+    return [(i + [None] * n)[:n] for i in items]
+
+
+def get_lens(items):
+    return list(map(lambda x: len(x), items))
 
 
 if __name__ == "__main__":
@@ -28,8 +40,12 @@ if __name__ == "__main__":
     )
     mode = "w"
     for df in df_iter:
-        vecs = pd.DataFrame(vectorize(tokenize(df[5])))
-        vecs.to_csv("./data/vectorized-smol.csv", header=None, index=False, mode=mode)
+        vecs = vectorize(tokenize(df[5]))
+        #  print(get_lens(vecs))
+        vecs_df = pd.DataFrame(make_even(vecs, 30))
+        vecs_df.to_csv(
+            "./data/vectorized-smol.csv", header=None, index=False, mode=mode
+        )
         n_done += CHUNK_SIZE
         mode = "a"
-        print(f"{n_done}...")
+        print(f"{n_done} ({max(get_lens(vecs))} tokens max)...")
