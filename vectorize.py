ac538cc0be655a3d83c254b90889d4d125d41f6a
Mikolaj Figurski <meek.f80@gmail.com>
2022-04-16 17:26:16 -0400

    Add sentiment to vectorization

--- vectorize.py
+++ vectorize.py
@@ -33,19 +33,21 @@ def get_lens(items):
     return list(map(lambda x: len(x), items))
 
 
+FNAME = "./data/smol.csv"
+OUT_FNAME = "./data/vectorized-smol.csv"
 if __name__ == "__main__":
     n_done = 0
     df_iter = pd.read_csv(
-        "./data/smol.csv", header=None, chunksize=CHUNK_SIZE, encoding="ISO-8859-1"
+        FNAME, header=None, chunksize=CHUNK_SIZE, encoding="ISO-8859-1"
     )
     mode = "w"
     for df in df_iter:
         vecs = vectorize(tokenize(df[5]))
         #  print(get_lens(vecs))
         vecs_df = pd.DataFrame(make_even(vecs, 30))
-        vecs_df.to_csv(
-            "./data/vectorized-smol.csv", header=None, index=False, mode=mode
-        )
+        vecs_df.insert(0, "Sentiment", df[0])
+        #  print(vecs_df)
+        vecs_df.to_csv(OUT_FNAME, header=None, index=False, mode=mode)
         n_done += CHUNK_SIZE
         mode = "a"
         print(f"{n_done} ({max(get_lens(vecs))} tokens max)...")
