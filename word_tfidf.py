728965a9c6e9aec0cb197668fe4ecd20ec98be21
Mikolaj Figurski <meek.f80@gmail.com>
2022-04-16 22:42:44 -0400

    Save dictionary, tfidf model

--- word_tfidf.py
+++ word_tfidf.py
@@ -20,38 +20,49 @@ def build_frequency_dict(l):
     return {tok: n for (tok, n) in d.items() if n > 1}
 
 
-def build_translation_dictionary(toks_list):
+def build_translation_dictionary(toks_list, save_name=None):
     toks_list = list(toks_list)
     freqs = build_frequency_dict(toks_list)
     #  print(len(freqs))
     toks_list = [toks for toks in toks_list]
     #  print(len(toks_list))
     d = corpora.Dictionary(toks_list)
+    if save_name:
+        d.save(save_name)
     return d
     #  corpus = [d.doc2bow(toks) for toks in toks_list]
     #  return corpus
 
 
-def build_tfidf(corpus):
+def build_tfidf(corpus, save_name=None):
     print("building tfidf")
     m = models.TfidfModel(corpus)
+    if save_name:
+        m.save(save_name)
     return m
 
 
+def read_translation_dictionary(fname):
+    return corpora.Dictionary.load(fname)
+
+
+def read_tfidf(fname):
+    return model.TfidfModel.load(fname)
+
+
 if __name__ == "__main__":
-    #  FNAME = "./data/training.1600000.processed.noemoticon.csv"
-    FNAME = "./data/smol.csv"
+    FNAME = "./data/training.1600000.processed.noemoticon.csv"
+    #  FNAME = "./data/smol.csv"
 
     # everything is being done with generators to minimize
     # memory usage. Important for large dataset
-    toks_gen = lambda: read_tokens_from_data(FNAME)  # iterator through FNAME tokens
+    toks_gen = lambda: read_tokens_from_data(FNAME)  # iterator through tweet tokens
 
-    d = build_translation_dictionary(toks_gen())
+    d = build_translation_dictionary(toks_gen(), "./models/dictionary.model")
     print(d)
-    corpus = [d.doc2bow(toks) for toks in toks_gen()]
-    m = build_tfidf(corpus)
-    print(m)
-    print(corpus[6])
-    print(m[corpus[6]])
+    corpus = (d.doc2bow(toks) for toks in toks_gen())
+    m = build_tfidf(corpus, "./models/word-tfidf.model")
+    #  print(corpus[6])
+    #  print(m[corpus[6]])
 
     #  print(len(freqs))
